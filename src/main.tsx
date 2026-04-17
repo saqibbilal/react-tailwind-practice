c8951832a9d558f8ada03fb8d6db94ae7f97ba64
Saqib Bilal <saqib_bilal786@yahoo.com>
2026-04-16 23:55:19 -0400

    chore: setting up react practice project 00-react-counter

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import './index.css';
 // Switch the project here
 // import App from './projects/01-qr-card/App';
 // import App from './projects/02-blog-preview-card/App';
-import App from './projects/03-bento-grid/App';
+// import App from './projects/03-bento-grid/App';
+import App from './projects/react/00-react-counter/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
--- src/projects/03-bento-grid/App.tsx
+++ src/projects/03-bento-grid/App.tsx
@@ -8,17 +8,30 @@ export default function App() {
                     <footer className="text-xl">Over 4,000 5-star reviews</footer>
                 </article>
 
-                <article className="bg-white flex flex-col justify-center items-center p-4 md:p-8 rounded-lg md:col-span-3 md:row-span-3 md:col-start-4">
-                    <img src="/bento-grid/images/illustration-multiple-platforms.webp" alt="Dashboard" className="w-full ml-45" />
-                    <span className="text-3xl font-semibold">Manage <br/>multiple <br/>accounts and platforms.</span>
+                <article className="bg-white p-4 md:p-8 rounded-lg md:col-span-3 md:row-span-3 md:col-start-4 relative overflow-hidden">
+                    <img src="/bento-grid/images/illustration-multiple-platforms.webp" alt="Dashboard" className="absolute left-20 top-4" />
+                    <span className="text-3xl font-semibold absolute bottom-4 leading-none tracking-tight">Manage <br/>multiple <br/>accounts and platforms.</span>
                 </article>
-                <article className="bg-[#f6ce6f] rounded-lg md:col-span-3 md:row-span-3">Maintain a consistent posting schedule.</article>
-                <article className="bg-[#dad1fb] rounded-lg md:col-span-3 md:row-span-7 md:row-start-1 md:col-start-10">
-                    Schedule to social media.
-                    Optimize post timings to publish content at the perfect time for your audience.</article>
-                <article className="bg-[#7252d9] rounded-lg md:col-span-6 md:col-start-7 md:row-start-8 md:row-span-3">Grow followers with non-stop content.</article>
-                <article className="bg-white rounded-lg md:col-span-3 md:row-span-3 md:row-start-8 md:col-start-4"> 56% faster audience growth</article>
-                <article className="bg-[#f8eee2] font-semibold text-4xl flex flex-col justify-center p-4 md:p-10 rounded-lg md:col-span-3 md:row-span-5 md:col-start-1 md:row-start-1">
+                <article className="bg-[#f6ce6f] p-4 md:p-6 rounded-lg md:col-span-3 md:row-span-3 overflow-hidden reletive">
+                    <span className="text-4xl font-semibold leading-[0.4] tracking-tighter">Maintain a consistent posting schedule.</span>
+                    <img src="/bento-grid/images/illustration-consistent-schedule.webp" alt="Dashboard"
+                         className="w-full"/>
+                </article>
+                <article className="bg-[#dad1fb] rounded-lg overflow-hidden flex flex-col items-center justify-center gap-4 p-4 md:p-10 md:col-span-3 md:row-span-7 md:row-start-1 md:col-start-10">
+                    <span className="text-4xl font-semibold leading-[0.8] tracking-tighter">Schedule to social media.</span>
+                    <img src="/bento-grid/images/illustration-schedule-posts.webp" alt="Dashboard" className="max-w-none w-[175%] -mr-[70%]" />
+                    <footer className="text-xl leading-none w-full">Optimize post timings to publish content at the perfect time for your audience.</footer>
+                </article>
+                <article className="bg-[#7252d9] rounded-lg p-4 md:p-5 flex flex-row gap-2 md:col-span-6 md:col-start-7 md:row-start-8 md:row-span-3">
+                    <img src="/bento-grid/images/illustration-grow-followers.webp" alt="Dashboard" className="" />
+                    <span className="text-5xl text-white flex items-center">Grow followers with non-stop content.</span>
+                </article>
+                <article className="bg-white flex flex-col justify-center pl-4 md:pl-10 gap-4 rounded-lg md:col-span-3 md:row-span-3 md:row-start-8 md:col-start-4">
+                    <span className="text-6xl font-semibold tracking-tighter"> &gt;56%</span>
+                    <footer className="text-lg">faster audience growth</footer>
+                    <img src="/bento-grid/images/illustration-audience-growth.webp" alt="Dashboard" className="w-[55%] mt-4" />
+                </article>
+                <article className="bg-[#f8eee2] leading-none tracking-tighter font-semibold text-4xl flex flex-col justify-center p-4 md:p-10 rounded-lg md:col-span-3 md:row-span-5 md:col-start-1 md:row-start-1">
                     <p>Create and schedule content <span className="text-purple-600/80 italic">quicker.</span></p>
                     <img src="/bento-grid/images/illustration-create-post.webp" alt="Dashboard" className="w-6/7 mt-4" />
                 </article>
