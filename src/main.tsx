69292dc1f2d3925f05c845be107c7ec0189a0fef
Saqib Bilal <saqib_bilal786@yahoo.com>
2026-04-17 04:19:21 -0400

    refactor: reorganize project structure into tailwind sub-directory

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 // Switch the project here
-// import App from './projects/01-qr-card/App';
-// import App from './projects/02-blog-preview-card/App';
-// import App from './projects/03-bento-grid/App';
+// import App from './projects/tailwind/01-qr-card/App';
+// import App from './projects/tailwind/02-blog-preview-card/App';
+// import App from './projects/tailwind/03-bento-grid/App';
 import App from './projects/react/00-react-counter/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
