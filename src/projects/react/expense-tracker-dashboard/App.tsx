68a3c31bc8a35b22a07cc85c4d65b9446a967cdb
Saqib Bilal <saqib_bilal786@yahoo.com>
2026-06-26 23:51:39 -0400

    feat: complete the Expense List functionality

--- src/projects/react/expense-tracker-dashboard/App.tsx
+++ src/projects/react/expense-tracker-dashboard/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 interface expenseProps {
     Description: string;
@@ -7,29 +7,75 @@ interface expenseProps {
 
 export default function ExpenseTrackerDashboard() {
     const [expenseList, setExpenseList] = useState<expenseProps[]>([{Description: "Basket of apples", Amount: 12.5}, {Description: "Bunch of Bananas", Amount: 1.5}]);
+    const [currentExpense, setCurrentExpense] = useState<expenseProps>({Description: "", Amount: 0});
+    const isEditting = useRef<null|number>(null);
+
+    const addExpense = (e:HTMLFormElement) => {
+        e.preventDefault();
+        if(!currentExpense.Description || !currentExpense.Amount) return;
+        if(isEditting.current !== null) {
+            const tempExpenseList = [...expenseList];
+            tempExpenseList[isEditting.current] = currentExpense;
+            setExpenseList(tempExpenseList);
+            isEditting.current = null;
+            setCurrentExpense({Description: "", Amount: 0});
+            return;
+        }
+        const tempExpenseList = [...expenseList, currentExpense];
+        setExpenseList(tempExpenseList)
+        setCurrentExpense({Description: "", Amount: 0});
+    }
+
+    const totalExpense = expenseList.reduce((acc, curr) => acc + curr.Amount, 0);
+
+    const cleanSlate = () => {
+        setExpenseList([]);
+        setCurrentExpense({Description: "", Amount: 0});
+        return;
+    }
+
+    const deleteExpense = (index:number) => {
+        const tempExpenseList = [...expenseList];
+        tempExpenseList.splice(index, 1);
+        setExpenseList(tempExpenseList);
+    }
+
+    const editExpense = (index:number) => {
+        const tempExpenseList = [...expenseList];
+        setCurrentExpense(tempExpenseList[index]);
+        isEditting.current = index;
+    }
+
     return (
         <div>
             <div className={`flex flex-col gap-2 items-center justify-center h-screen bg-gray-900 text-white text-center`}>
                 <div>
                     <h1 className={`text-3xl font-bold p-10`}>Expense Tracker Dashboard</h1>
                     <div className={`flex flex-row gap-2 mx-4 items-center justify-between`}>
-                        <h1>Total Expense: {expenseList.reduce((total:number, expenseEntry:expenseProps) => {
-                            return total + expenseEntry.Amount;
-                        },0)}</h1>
+                        <h1>Total Expense: {totalExpense} {totalExpense > 1000 && (<span className="text-red-500"> (Budget Exceeded!)</span>)}</h1>
                         <h1>Number of expenses: {expenseList.length}</h1>
                     </div>
 
                     <div>
-                        <form>
+                        <form onSubmit={addExpense}>
                             <input
                                 className={`bg-white rounded-md m-4 text-gray-700 p-2 hover:border-gray-900`}
                                 type="text"
+                                value = {currentExpense.Description}
                                 placeholder="Enter expense name"
+                                onChange={(e)=>{
+                                    setCurrentExpense({...currentExpense, Description: e.target.value});
+
+                                }}
                             />
                             <input
                                 className={`bg-white rounded-md m-4 text-gray-700 p-2 hover:border-gray-900`}
                                 type="number"
+                                min="0"
+                                step="0.01"
                                 placeholder="Enter expense amount"
+                                value = {currentExpense.Amount}
+                                onChange = {(e)=>{setCurrentExpense({...currentExpense, Amount: e.target.valueAsNumber})}}
                             />
                             <button className={`bg-blue-500 rounded-md m-4 hover:border-gray-900 p-2`} type="submit">Add Expense</button>
                         </form>
@@ -40,14 +86,15 @@ export default function ExpenseTrackerDashboard() {
                     {expenseList.map((expenseEntry, index) => (
 
                         <li key={index} className={`flex flex-row gap-4 m-2 items-center justify-between w-full font-bold text-xl`}>
-                            <span>{expenseEntry.Description}:{expenseEntry.Amount}</span>
-                            <span>Delete</span>
+                            <span>{expenseEntry.Description}: ${expenseEntry.Amount}</span>
+                            <span className={`text-green-400 hover:text-green-600`} onClick={()=>{editExpense(index)}}>Edit</span>
+                            <span className={`text-red-400 hover:text-red-600`} onClick={()=>{deleteExpense(index)}}>Delete</span>
                         </li>
 
                     ))}
                 </div>
 
-                <button className={`bg-slate-500 rounded-md p-2`}>Clean Slate</button>
+                <button className={`bg-slate-500 rounded-md p-2`} onClick={cleanSlate}>Clean Slate</button>
             </div>
         </div>
     )
